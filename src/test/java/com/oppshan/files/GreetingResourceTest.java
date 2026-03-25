c7cdc95d3c66930bcff31dda6b1cfe86d728f8d0
Warren Nocos <warren@isr.com.ph>
2026-03-26 02:23:40 +0800

    refs #1 Set up code coverage

--- src/test/java/com/oppshan/files/GreetingResourceTest.java
+++ src/test/java/com/oppshan/files/GreetingResourceTest.java
@@ -3,18 +3,15 @@ package com.oppshan.files;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
-import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 @QuarkusTest
 class GreetingResourceTest {
+
     @Test
     void testHelloEndpoint() {
-        given()
-                .when().get("/hello")
-                .then()
-                .statusCode(200)
-                .body(is("Hello from Quarkus REST"));
+        assertThat(new GreetingResource().hello(), is("Hello from Quarkus REST"));
     }
 
 }
